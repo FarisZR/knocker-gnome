9ba840a74139543172b39babc1727f6743389a46
FarisZR <35614734+FarisZR@users.noreply.github.com>
2025-10-30 17:48:59 +0100

    migrate eslint to v9 and update config

---
+++ eslint.config.js
@@ -0,0 +1,63 @@
+import js from '@eslint/js';
+
+const baseGlobals = {
+    imports: 'readonly',
+    log: 'readonly',
+    logError: 'readonly',
+    print: 'readonly',
+    printerr: 'readonly',
+    ARGV: 'readonly',
+    _: 'readonly',
+};
+
+export default [
+    js.configs.recommended,
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module',
+            globals: baseGlobals,
+        },
+        rules: {
+            indent: ['error', 4, { SwitchCase: 1 }],
+            'linebreak-style': ['error', 'unix'],
+            quotes: ['error', 'single', { avoidEscape: true }],
+            semi: ['error', 'always'],
+            'no-unused-vars': ['warn', {
+                argsIgnorePattern: '^_',
+                varsIgnorePattern: '^_',
+            }],
+            'no-trailing-spaces': 'error',
+            'eol-last': ['error', 'always'],
+            'no-multiple-empty-lines': ['error', { max: 1 }],
+            'comma-dangle': ['error', 'always-multiline'],
+            'arrow-parens': ['error', 'as-needed'],
+            'prefer-const': 'warn',
+            'no-var': 'error',
+        },
+    },
+    {
+        files: ['prefs.js'],
+        languageOptions: {
+            globals: {
+                ...baseGlobals,
+                Adw: 'readonly',
+                Gio: 'readonly',
+                Gtk: 'readonly',
+            },
+        },
+    },
+    {
+        files: ['extension.js', 'knocker*.js'],
+        languageOptions: {
+            globals: {
+                ...baseGlobals,
+                GLib: 'readonly',
+                GObject: 'readonly',
+                Gio: 'readonly',
+                console: 'readonly',
+            },
+        },
+    },
+];
